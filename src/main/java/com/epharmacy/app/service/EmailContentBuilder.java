fd2da69fee4824bc5ba94d5a4b74092147a390f6
Abdelghani ELAOUD <abdelghanielaoud@gmail.com>
2024-06-05 01:09:00 +0200

    Added the update password mechanism

--- src/main/java/com/epharmacy/app/service/EmailContentBuilder.java
+++ src/main/java/com/epharmacy/app/service/EmailContentBuilder.java
@@ -12,7 +12,7 @@ public class EmailContentBuilder {
                 "<p>We received a request to reset your password. Click the button below to reset it:</p>" +
                 "<a href=\"" + resetLink + "\" style=\"display: inline-block; padding: 10px 20px; font-size: 16px; color: #ffffff; background-color: #007bff; text-decoration: none; border-radius: 5px;\">Reset Password</a>" +
                 "<p>If you didn't request a password reset, please ignore this email.</p>" +
-                "<p>Thank you,<br>Your Company Name</p>" +
+                "<p>Thank you,<br>E-Pharmacy</p>" +
                 "</body>" +
                 "</html>";
     }
